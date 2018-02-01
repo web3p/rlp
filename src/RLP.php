a8d1819ad407103ca3648a930056c9c2a2464fac
sc0Vu <alk03073135@gmail.com>
2018-02-01 11:23:32 +0800

    Add rlp encode.

--- src/RLP.php
+++ src/RLP.php
@@ -16,4 +16,135 @@ use RLP\Buffer;
 
 class RLP
 {
+    /**
+     * encode
+     * 
+     * @param array $inputs array of string
+     * @return string
+     */
+    public function encode($inputs)
+    {
+        if (!is_array($inputs)) {
+            throw new InvalidArgumentException('Inputs must be array when call encode.');
+        }
+        $output = new Buffer;
+
+        foreach ($inputs as $input) {
+            if (is_array($input)) {
+                $output->concat($this->encode($input));
+            } else {
+                $input = $this->toBuffer($input);
+                $length = $input->length();
+
+                if ($length === 1) {
+                    $output[] = $input[0];
+                } else {
+                    $output->concat($this->encodeLength($length, 128), $input);
+                }
+            }
+        }
+        $result = new Buffer;
+        return $result->concat($this->encodeLength($output->length(), 192), $output);
+    }
+
+    /**
+     * encodeLength
+     * 
+     * @param int $length
+     * @param int $offset
+     * @return \RLP\Buffer
+     */
+    protected function encodeLength($length, $offset)
+    {
+        if (!is_int($length) || !is_int($offset)) {
+            throw new InvalidArgumentException('Length and offset must be int when call encodeLength.');
+        }
+        if ($length < 56) {
+            return new Buffer($this->intToHex($length), 'hex');
+        }
+        $hexLength = $this->intToHex($length);
+        $firstByte = $this->intToHex($offset + 55 + (strlen($hexLength) / 2));
+        var_dump($firstByte . $hexLength);
+        return new Buffer($firstByte . $hexLength, 'hex');
+    }
+
+    /**
+     * intToHex
+     * 
+     * @param int $value
+     * @return string
+     */
+    protected function intToHex($value)
+    {
+        if (!is_int($value)) {
+            throw new InvalidArgumentException('Value must be int when call intToHex.');
+        }
+        $hex = dechex($value);
+
+        return $this->padToEven($hex);
+    }
+
+    /**
+     * padToEven
+     * 
+     * @param string $value
+     * @return string
+     */
+    protected function padToEven($value)
+    {
+        if (!is_string($value)) {
+            throw new InvalidArgumentException('Value must be string when call padToEven.');
+        }
+        if ((strlen($value) % 2) !== 0 ) {
+            $value = '0' . $value;
+        }
+        return $value;
+    }
+
+    /**
+     * toArray
+     * Format input to value, deprecated when we have toBuffer.
+     * 
+     * @param mixed $input
+     * @return array
+     */
+    // protected function toArray($input)
+    // {
+    //     if (is_string($input)) {
+    //         if (strpos($input, '0x') === 0) {
+    //             // hex string
+    //             $value = str_replace('0x', '', $input);
+    //             return $input;
+    //         } else {
+    //             return str_split($input, 1);
+    //         }
+    //     }
+    //     throw new InvalidArgumentException('The input type didn\'t support.');
+    // }
+
+    /**
+     * toBuffer
+     * Format input to buffer.
+     * 
+     * @param mixed $input
+     * @return \RLP\Buffer
+     */
+    protected function toBuffer($input)
+    {
+        if (is_numeric($input)) {
+            return new Buffer($input);
+        } elseif (is_string($input)) {
+            if (strpos($input, '0x') === 0) {
+                // hex string
+                $input = str_replace('0x', '', $input);
+                return new Buffer($input, 'hex');
+            }
+            return new Buffer(str_split($input, 1));
+        } elseif (is_array($input)) {
+            return new Buffer($input);
+        } elseif ($input instanceof Buffer) {
+            return $input;
+        }
+        throw new InvalidArgumentException('The input type didn\'t support.');
+    }
 }
---
+++ test/unit/EncodeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Test\Unit;
+
+use Test\TestCase;
+
+class EncodeTest extends TestCase
+{
+    public function testEncode()
+    {
+        $rlp = $this->rlp;
+
+        $encoded = $rlp->encode(['dog', 'god', 'cat']);
+        $this->assertEquals('0c03646f6703676f6403636174', $encoded->toString('hex'));
+        $this->assertEquals(13, $encoded->length());
+
+        $encoded = $rlp->encode(['0xabcd', '0xdeff', '0xaaaa']);
+        $this->assertEquals('0902abcd02deff02aaaa', $encoded->toString('hex'));
+        $this->assertEquals(10, $encoded->length());
+    }
+}
